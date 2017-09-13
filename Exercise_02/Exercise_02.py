fbeb65f2ac7e89237b2ed0903e0287978593e16a
matrixk <matrixk@outlook.com>
2017-09-13 14:41:00 +0800

    上传第二次作业

---
+++ Exercise_02/Exercise_02.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#coding:utf-8
+'''
+输出字母拼图“禾城”
+（不想拼真名，拼个网名就算了……）
+'''
+
+h = '''               
+               
+           ;L  
+  WWWWWWWWWWD  
+       W       
+       W       
+ EWWWWWWWWWWWD 
+ :::::WWW::::. 
+     KLWEt     
+    ,W W W     
+    W  W iW    
+  fW   W  .W,  
+ WW.   W   iWW 
+ j     W       
+       W       '''
+c = '''               
+   ,     ,G;   
+   W     ;KWW  
+   W  :::iW::: 
+ ,WWW:WWWWWWWK 
+   W :.   W    
+   W :EEE W Wj 
+   W ,. W W W  
+   W i  W WW   
+  LWWD  W WE   
+ E   WWWW.W. W 
+    ,W.: W W W 
+    W   W  fWi 
+               '''
+print h + c
